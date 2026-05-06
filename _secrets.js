58f78347723a348a871ccf5bf0acaa13dfc03c10
infoarifoma-cell <infoarifoma@gmail.com>
2026-05-06 14:02:41 +0100

    Add _secrets.js with BC and Supabase env vars for Vercel

---
+++ _secrets.js
@@ -0,0 +1,7 @@
+// Secrets inyectados por Vercel (environment variables)
+window.__SUPABASE_URL__ = 'https://bnsfgzjqmibsrklllqxb.supabase.co';
+window.__SUPABASE_KEY__ = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJuc2ZnempxbWlic3JrbGxzcXhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNzYwNzksImV4cCI6MjA4OTk1MjA3OX0.8mTQHPdO954ICBd1Xam-kKmcA69CMyO2v3x1liFgWyk';
+window.__BC_TENANT__ = '5bd828f2-1899-48ba-a269-c37733f41806';
+window.__BC_CLIENT__ = 'e2a57ff0-8ea7-433d-a2af-7335d3f01847';
+window.__BC_ENV__ = 'Production';
+window.__BC_COMPANY__ = 'ARIFOMA 25P.V06';
