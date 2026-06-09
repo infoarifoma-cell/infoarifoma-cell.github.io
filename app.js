37ac7398c359c97a8f2823c1f1cbe5a15d6b5626
infoarifoma-cell <infoarifoma@gmail.com>
2026-06-09 13:32:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -10355,10 +10355,10 @@ async function _ensayosGenerarPDF() {
 
     // Coordenadas exactas por ensayo+fracción
     const COORDS = {
-      'granulometria|0/4':    {x:310, y:604},
-      'granulometria|4/12':   {x:335, y:604},
-      'granulometria|12/20':  {x:360, y:604},
-      'granulometria|20/40':  {x:385, y:604},
+      'granulometria|0/4':    {x:310, y:614},
+      'granulometria|4/12':   {x:335, y:614},
+      'granulometria|12/20':  {x:360, y:614},
+      'granulometria|20/40':  {x:385, y:614},
       'cont_finos|0/4':       {x:310, y:610},
       'cont_finos|4/12':      {x:335, y:610},
       'cont_finos|12/20':     {x:360, y:610},
